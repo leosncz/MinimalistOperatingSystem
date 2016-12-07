81437750dffdbd1890986b4ec0031b16f50788a4
leosncz <leosa@hotmail.fr>
2016-12-07 14:01:53 +0100

    Update main.asm

--- bootloader/main.asm
+++ bootloader/main.asm
@@ -1,5 +1,11 @@
-```asm
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;Bootloader of MOS Operating System;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-```
+[org 0x7C00]
+jmp main
+%include "func.asm" ;Contains usefull functions.
+main:
+jmp $ ;Loop.
+times 510 - ($-$$) db 0 ;Must be 512 size.
+db 0x55 ;Magical Number.
+db 0xAA ;Magical Number.
