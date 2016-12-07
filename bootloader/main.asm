60ee9e7b67f407f9b4ef67b2db46312c5ffb274c
leosncz <leosa@hotmail.fr>
2016-12-07 14:05:51 +0100

    Update main.asm

--- bootloader/main.asm
+++ bootloader/main.asm
@@ -1,11 +1,10 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;Bootloader of MOS Operating System;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Bootloader of MOS Operating System.
+; NASM syntax.
 [org 0x7C00]
 jmp main
-%include "func.asm" ;Contains usefull functions.
+%include "func.asm" ; Contains usefull functions.
 main:
 jmp $ ;Loop.
-times 510 - ($-$$) db 0 ;Must be 512 size.
-db 0x55 ;Magical Number.
-db 0xAA ;Magical Number.
+times 510 - ($-$$) db 0 ; Must be 512 size.
+db 0x55 ; Magical Number.
+db 0xAA ; Magical Number.
