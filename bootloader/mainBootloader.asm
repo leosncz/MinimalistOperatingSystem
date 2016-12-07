c009025033f86e024b31cda87b22d769e4ffad6d
leosncz <leosa@hotmail.fr>
2016-12-07 20:47:15 +0100

    Update mainBootloader.asm

--- bootloader/mainBootloader.asm
+++ bootloader/mainBootloader.asm
@@ -1,6 +1,7 @@
 ; Bootloader of MOS Operating System.
 ; Check the license at GitHub.com/leosncz/OperatingSystem/
 ; NASM syntax.
+; This code loads in memory the second and third sectors at 0x0:0x1000 (real mode addressing).
 [org 0x7C00]
 jmp main
 %include "func.asm" ; Contains usefull functions.
