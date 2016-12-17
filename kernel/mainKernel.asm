dba4d7b19f866e07de7d3e090a3346a24fc8f4bb
leosncz <leosa@hotmail.fr>
2016-12-17 18:23:12 +0100

    Update mainKernel.asm

--- kernel/mainKernel.asm
+++ kernel/mainKernel.asm
@@ -1,3 +1,7 @@
+; Kernel of MOS Operating System.
+; Check the license at GitHub.com/leosncz/OperatingSystem/
+; NASM syntax.
+; This is the first code loaded by the bootloader on the second sector.
 [org 0x1000]
 mov ah, 0x0E
 mov al, [letterX]
