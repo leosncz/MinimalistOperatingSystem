b8acf1c435ae2301fa0e7107c5678c9847106e22
leosncz <leosa@hotmail.fr>
2016-12-17 23:43:04 +0100

    Update resources.asm

--- bootloader/resources.asm
+++ bootloader/resources.asm
@@ -1,60 +1,32 @@
 ; Real Mode things of MOS Operating System.
-
 ; Check the license at GitHub.com/leosncz/OperatingSystem/
-
 ; NASM syntax.
-
-
 init:
-
 mov [bootDriveNumber], dl ; Save boot drive number id.
-
 xor ax, ax
-
 mov dx, ax
-
 ret
 
 printInt: ; Print the SI register to the screen.
-
 mov ah, 0x2
-
 mov bh, 0x0
-
 int 0x10
-
 mov bl, 0010b ; Binary expressed color of the text.
-
 mov cx, 1d
-;
 mov ah, 0x9
-
 mov al, [si]
-
 cmp al, 0
-
 je endPrintInt
-
 int 0x10
-
 inc si
-
 inc dl ; Add a row.
-
 jmp printInt
-
 endPrintInt:
-
 ret
 
-
-
 printIntPrepareNL: ; Add new line.
-
 inc dh
-
 mov dl, 0d
-
 ret
 
 load2nd3rdSector:
@@ -79,9 +51,7 @@ pop ax
 ret
 
 bootDriveNumber: db 0d ; Boot drive number ID.
-
 text1: db "Minimalist Operating System (MOS) ALPHA bootloader", 0
-
 text2: db "Loading Second stage bootloader at 0x0:0x1000 ...", 0
 text3: db "Second Stage Loaded !", 0
 text4: db "Jumping into hyperspace (PMode in fact ...)", 0
