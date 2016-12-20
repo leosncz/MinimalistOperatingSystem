9bd41bfdff7682ec812b97f6ec2aa95e7a7c7c4e
leosncz <leosa@hotmail.fr>
2016-12-20 10:48:53 +0100

    Update secondStageBootloader.asm

--- Bootloader/secondStageBootloader.asm
+++ Bootloader/secondStageBootloader.asm
@@ -1,9 +1,6 @@
 ; Second Stage Bootloader of MOS Operating System.
-
 ; Check the license at GitHub.com/leosncz/OperatingSystem/
-
 ; NASM syntax.
-
 [org 0x500]
 jmp main
 %include "resources.asm"
@@ -33,7 +30,6 @@ mov fs , ax
 mov gs , ax
 mov ebp , 0x90000 ; Update our stack position so it is right
 mov esp , ebp ; at the top of the free space.
-
 mov cx, 0x3C0
 mov si, text6
 call writeGreenSIToVideoMemory
