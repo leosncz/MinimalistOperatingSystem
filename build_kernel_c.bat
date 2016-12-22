a639d0a54cb14802e7450182f35e46cd9bb510d9
leosncz <leosa@hotmail.fr>
2016-12-22 16:44:57 +0100

    Update build_kernel_c.bat

--- build_kernel_c.bat
+++ build_kernel_c.bat
@@ -1,3 +1,4 @@
+# This code build the file kernel.c and convert it into a raw binary code.
 gcc -ffreestanding -c kernel.c -o kernel.o
 ld -T NUL -o kernel.tmp -Ttext 0x7e00 kernel.o
 objcopy -O binary -j .text  kernel.tmp kernel.bin
