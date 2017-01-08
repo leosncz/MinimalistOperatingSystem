c57f68cb6f7f2de8e8fabef9bb42a3f6254c07a7
leosncz <leosa@hotmail.fr>
2017-01-08 11:19:18 +0100

    Update kernel32.c

--- kernel/kernel32.c
+++ kernel/kernel32.c
@@ -1,6 +1,13 @@
+/*
+File: kernel32.c
+Author: leosncz
+Purpose:
+This is the kernel's main entry point.
+*/
 #include "mos_kernel32_basic.h"
 void kmain()
 {
-printCharacter(0);
-while(1){}; // Loop.
+unsigned char character = 'A';
+printRedCharacter(character);
+while(1){}; // Infinite loop.
 }
