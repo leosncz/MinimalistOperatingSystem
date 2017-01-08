80f746963ba40fdac0fd9566f5bcf61bb4effdfe
leosncz <leosa@hotmail.fr>
2017-01-08 11:18:05 +0100

    Update mos_kernel32_basic.h

--- kernel/mos_kernel32_basic.h
+++ kernel/mos_kernel32_basic.h
@@ -5,11 +5,11 @@ Purpose:
 This file contains datas and functions the kernel might uses.
 */
 #define RAMSCREEN 0xB8000 // Video address.
-void printCharacter(int offset)
+void printRedCharacter(unsigned char characterToPrint)
 {
 unsigned char *video = (unsigned char*)0xB8000;
-video=(unsigned char*)0xB8000+offset;
-char character = 'A';
+video=(unsigned char*)0xB8000;
+unsigned char character = characterToPrint;
 *video=character;
 video = (unsigned char*)0xB8001;
 *video = 0xC;
