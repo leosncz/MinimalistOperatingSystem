a0cb540584eb14739411c697960854358bfa0b6a
leosncz <leosa@hotmail.fr>
2017-01-08 12:49:11 +0100

    Update build_kernel_c.bat

--- kernel/build_kernel_c.bat
+++ kernel/build_kernel_c.bat
@@ -3,7 +3,8 @@ nasm kernel_entry.asm -f elf -o kernel_entry.o
 gcc -ffreestanding -c kernel32.c -o kernel32.o
 ld -T NUL -o kernel32.tmp -Ttext 0x7e00 kernel_entry.o kernel32.o
 objcopy -O binary -j .text  kernel32.tmp kernel32.bin
+:: Delete useless things.
 del kernel32.tmp
 del kernel32.o
 del kernel_entry.o
-pause
+pause :: To see if it worked.
