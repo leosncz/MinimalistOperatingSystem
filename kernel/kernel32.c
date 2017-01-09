bed1dd8f64799985d11b93fe005cef78874c98e4
leosncz <leosa@hotmail.fr>
2017-01-09 19:30:13 +0100

    Update kernel32.c

--- kernel/kernel32.c
+++ kernel/kernel32.c
@@ -9,10 +9,11 @@ void kmain() // Kernel's main entry point.
 {
 	char string1[21] = "Kernel>MOS's kernel loaded !";
 	char string2[39] = "Kernel>Actual MOS's kernel version is  ";
-	string2[39] = MOS_
+	string2[39] = MOS_KERNEL_VERSION;
 	char string3[32] = "Kernel>MOS is waiting for something ...";
 	clearScreen(); // Clear the screen.
 	printGreenString(0,0,string1,21);
-	printWhiteString(0,1,string2,32);	
+	printGreenString(0,1,string2,39);
+	printWhiteString(0,1,string3,32);	
 	while(1){}; // Infinite loop.
 }
