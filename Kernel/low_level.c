afaa912e7deb58da5caa7a08d4cb4188d264c1be
leosncz <leosa@hotmail.fr>
2017-01-14 18:46:33 +0100

    Update low_level.c

--- Kernel/low_level.c
+++ Kernel/low_level.c
@@ -1,33 +1,34 @@
 /*
 File: low_level.c
-Author: leosncz (inspired by Nick blundell)
+Author: leosncz
 Purpose:
 Contains low level functions.
 */
-unsigned char port_byte_in ( unsigned short port ) 
+unsigned char read_port_byte(unsigned short port) 
 {
 // A handy C wrapper function that reads a byte from the specified port
 // "=a" ( result ) means : put AL register in variable RESULT when finished
 // "d" ( port ) means : load EDX with port
-unsigned char result ;
-__asm__ (" in %% dx , %% al " : "=a " ( result ) : "d " ( port ));
-return result ;
+  unsigned char result ;
+  __asm__ (" in %% dx , %% al " : "=a " ( result ) : "d " ( port ));
+  return result ;
 }
 
-void port_byte_out ( unsigned short port , unsigned char data )
+void write_port_byte(unsigned short port , unsigned char data)
 {
-// "a" ( data ) means : load EAX with data
-// "d" ( port ) means : load EDX with port
-__asm__ (" out %% al , %% dx " : :" a" ( data ), "d" ( port ));
+  // "a" ( data ) means : load EAX with data
+  // "d" ( port ) means : load EDX with port
+  __asm__ (" out %% al , %% dx " : :" a" ( data ), "d" ( port ));
 }
 
-unsigned short port_word_in ( unsigned short port ) 
+unsigned short read_port_word(unsigned short port) 
 {
-unsigned short result ;
-__asm__ (" in %% dx , %% ax " : "=a " ( result ) : "d " ( port ));
-return result ;
+  unsigned short result ;
+  __asm__ ("in %% dx , %% ax " : "=a " ( result ) : "d " ( port ));
+  return result ;
 }
 
-void port_word_out ( unsigned short port , unsigned short data ) {
-__asm__ (" out %% ax , %% dx " : :" a" ( data ), "d" ( port ));
+void write_port_word(unsigned short port , unsigned short data)
+{
+  __asm__ (" out %% ax , %% dx " : :" a" ( data ), "d" ( port ));
 }
