bfb4de47b2c053151bd9f4caa0f9d5653154d5ed
leosncz <leosa@hotmail.fr>
2017-01-14 19:05:37 +0100

    Update low_level.c

--- Kernel/low_level.c
+++ Kernel/low_level.c
@@ -6,9 +6,9 @@ Contains low level functions.
 */
 unsigned char read_port_byte(unsigned short port) 
 {
-// A handy C wrapper function that reads a byte from the specified port
-// "=a" ( result ) means : put AL register in variable RESULT when finished
-// "d" ( port ) means : load EDX with port
+  // A handy C wrapper function that reads a byte from the specified port
+  // "=a" ( result ) means : put AL register in variable RESULT when finished
+  // "d" ( port ) means : load EDX with port
   unsigned char result ;
   __asm__ ("in %% dx , %% al " : "=a " ( result ) : "d " ( port ));
   return result ;
