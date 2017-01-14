b7a7606a2a25e11ee897d3a019ff5b3b2bee0bcc
leosncz <leosa@hotmail.fr>
2017-01-14 18:43:47 +0100

    Add files via upload

---
+++ Kernel/low_level.c
@@ -0,0 +1,33 @@
+/*
+File: low_level.c
+Author: leosncz (inspired by Nick blundell)
+Purpose:
+Contains low level functions.
+*/
+unsigned char port_byte_in ( unsigned short port ) 
+{
+// A handy C wrapper function that reads a byte from the specified port
+// "=a" ( result ) means : put AL register in variable RESULT when finished
+// "d" ( port ) means : load EDX with port
+unsigned char result ;
+__asm__ (" in %% dx , %% al " : "=a " ( result ) : "d " ( port ));
+return result ;
+}
+
+void port_byte_out ( unsigned short port , unsigned char data )
+{
+// "a" ( data ) means : load EAX with data
+// "d" ( port ) means : load EDX with port
+__asm__ (" out %% al , %% dx " : :" a" ( data ), "d" ( port ));
+}
+
+unsigned short port_word_in ( unsigned short port ) 
+{
+unsigned short result ;
+__asm__ (" in %% dx , %% ax " : "=a " ( result ) : "d " ( port ));
+return result ;
+}
+
+void port_word_out ( unsigned short port , unsigned short data ) {
+__asm__ (" out %% ax , %% dx " : :" a" ( data ), "d" ( port ));
+}
