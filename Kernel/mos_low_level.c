a389c278dbe5f08737afca1616b3244a8c92eb19
leosncz <leosa@hotmail.fr>
2017-01-28 15:02:11 +0100

    Update mos_low_level.c

--- Kernel/mos_low_level.c
+++ Kernel/mos_low_level.c
@@ -4,6 +4,8 @@ Author: leosncz
 Purpose:
 Contains low level functions.
 */
+#define CRT_PORT_DATA 0x3D4
+#define CRT_PORT_INDEX 0x3D5
 void outb(unsigned int portToWrite, unsigned char whatToPrint)
 {
   asm volatile("outb %%al,%%dx": :"d" (portToWrite), "a" (whatToPrint));
