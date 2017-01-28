863371ec2697e858ec466df036d60a88f9d0de40
leosncz <leosa@hotmail.fr>
2017-01-28 18:51:08 +0100

    Update mos_kernel32_basic.c

--- Kernel/mos_kernel32_basic.c
+++ Kernel/mos_kernel32_basic.c
@@ -8,6 +8,5 @@ This file contains datas and functions the kernel might uses.
 #define MOS_KERNEL32_BASIC
 #define RAMSCREEN 0xB8000 // Video address.
 #define MOS_KERNEL_VERSION "ALPHA" // Must contains five characters. For example if version is 1.0 : #define MOS_KERNEL_VERSION "1.000"
-#include "mos_low_level.c" // Contains low level functions.
 #include "mos_graphics_text_mode.c"
 #endif
