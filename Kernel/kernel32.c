036040fd6e49651dbf461f8aad1f2edb25f0f3e6
leosncz <leosa@hotmail.fr>
2017-01-30 19:05:38 +0100

    Update kernel32.c

--- Kernel/kernel32.c
+++ Kernel/kernel32.c
@@ -7,14 +7,5 @@ This is the kernel's main entry point.
 #include "mos_kernel32_basic.c"
 void kmain() // Kernel's main entry point.
 {
-	clearScreen(); // Clear bootloader outputs.
-	char string1[31] = "[KERNEL LOG]MOS Kernel loaded !";
-	char string2[30] = "[KERNEL LOG]Kernel version is ";
-	char string3[5] = MOS_KERNEL_VERSION;
-	char string4[47] = "[KERNEL LOG]Kernel is waiting for something ...";
-	kernelOutputGreenString(0,0,string1,31);
-	kernelOutputGreenString(0,1,string2,30);
-	kernelOutputRedString(30,1,string3,5);
-	kernelOutputWhiteString(0,2,string4,47);
 	while(1){}; // Infinite loop (so it does not execute other things).
 }
