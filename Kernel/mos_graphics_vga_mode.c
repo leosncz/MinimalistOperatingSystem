e7c0d7955f8b43d296dd7673db81051e2a165164
leosncz <leosa@hotmail.fr>
2017-01-30 19:35:28 +0100

    Update mos_graphics_vga_mode.c

--- Kernel/mos_graphics_vga_mode.c
+++ Kernel/mos_graphics_vga_mode.c
@@ -7,12 +7,16 @@ ONLY VGA MODE graphics stuff.
 #ifndef MOS_GRAPHICS_VGA_MODE
 #define MOS_GRAPHICS_VGA_MODE
 #include "mos_low_level.c"
-void writePixelVGA_test()
+// Define some colors.
+#define VGA_COLOR_BLACK 0x0
+#define VGA_COLOR_RED 0x28
+#define VGA_COLOR_GREEN 0x2F
+#define VGA_COLOR_WHITE 0xF
+#define VGA_COLOR_GREY 0x18
+// There are 256 colors in total(one byte), feel free to add some colors if you want.
+void writePixelVGA(char x, char y, char color)
 {
-    unsigned char* location = (unsigned char*)0x0A0000;
-    writeByteToMemory(0xF,location);
-    writeByteToMemory(0xF,location+1);
-    writeByteToMemory(0xF,location+2);
-    writeByteToMemory(0xF,location+3);
+    unsigned char* location = (unsigned char*)0x0A0000; // This is the base address.
+    writeByteToMemory(color,location + ((320*y) + x));
 }
 #endif
