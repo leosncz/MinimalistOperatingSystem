fad37f5baaa7ee37cf35169803351452f5e96167
leosncz <leosa@hotmail.fr>
2017-01-30 19:06:01 +0100

    Update mos_kernel32_basic.c

--- Kernel/mos_kernel32_basic.c
+++ Kernel/mos_kernel32_basic.c
@@ -7,5 +7,5 @@ This file contains datas and functions the kernel might uses.
 #ifndef MOS_KERNEL32_BASIC
 #define MOS_KERNEL32_BASIC
 #define MOS_KERNEL_VERSION "ALPHA" // Must contains five characters. For example if version is 1.0 : #define MOS_KERNEL_VERSION "1.000"
-#include "mos_graphics_text_mode.c"
+#include "mos_graphics_vga_mode.c"
 #endif
