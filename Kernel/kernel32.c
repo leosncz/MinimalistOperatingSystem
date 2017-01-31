79132a7258609e23423f80013f303f6365162c1c
leosncz <leosa@hotmail.fr>
2017-01-31 21:06:42 +0100

    Update kernel32.c

--- Kernel/kernel32.c
+++ Kernel/kernel32.c
@@ -9,7 +9,7 @@ void kmain() // Kernel's main entry point.
 {
 	/* 
 	We are in VGA Mode (NOT IN TEXT MODE !).
-	Let's print some pixel to prove that the kernel has been loaded.
+	Let's print some pixel to prove the kernel has been loaded.
 	*/
 	writePixelVGA(0,0,VGA_COLOR_WHITE); // Let's prove that kernel is loaded.
 	while(1){}; // Infinite loop (so it does not execute other things).
