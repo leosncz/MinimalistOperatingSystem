94ba1f30f9599ae45a72c0a950872e6ebf4f726d
leosncz <leosa@hotmail.fr>
2017-02-01 20:26:10 +0100

    Update kernel32.c

--- Kernel/kernel32.c
+++ Kernel/kernel32.c
@@ -11,6 +11,6 @@ void kmain() // Kernel's main entry point.
 	We are in VGA Mode (NOT IN TEXT MODE !).
 	The VGA mode has been set up by our bootloader.
 	*/
-	writePixelVGA(0,0,VGA_COLOR_WHITE); // Print a pixel to prove the kernel is loaded.
+	writeRightLineVGA(5,5,50,5,VGA_COLOR_RED);
 	while(1){}; // Infinite loop (so it does not execute other things).
 }
