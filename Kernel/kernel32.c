0f7f862f03e2efe8ef3b2f9e50802d720c8e8c9f
leosncz <leosa@hotmail.fr>
2017-02-12 15:06:11 +0100

    Update kernel32.c

--- Kernel/kernel32.c
+++ Kernel/kernel32.c
@@ -11,5 +11,12 @@ void kmain() // Kernel's main entry point.
 	We are in VGA Mode (NOT IN TEXT MODE !).
 	The VGA mode has been set up by our bootloader.
 	*/
+	
+	#if(DEV_STATUS == DEBUG_MODE) // We are in DEBUG mode.
+	#endif
+	
+	#if(DEV_STATUS == RELEASE_MODE) // We are in RELEASE mode.
+	#endif
+	
 	while(1){}; // Infinite loop (so it does not execute other things).
 }
