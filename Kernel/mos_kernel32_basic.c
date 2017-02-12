010291ae9c8ae0a256162fb41b2d4acf4329553c
leosncz <leosa@hotmail.fr>
2017-02-12 15:01:34 +0100

    Update mos_kernel32_basic.c

--- Kernel/mos_kernel32_basic.c
+++ Kernel/mos_kernel32_basic.c
@@ -7,6 +7,8 @@ This file contains datas and functions the kernel might uses.
 #ifndef MOS_KERNEL32_BASIC
 #define MOS_KERNEL32_BASIC
 #define MOS_KERNEL_VERSION "ALPHA" // Must contains five characters. For example if version is 1.0 : #define MOS_KERNEL_VERSION "1.000"
-#define DEV_STATUS 0 // 0 : debug  1 : release
+#define DEBUG_MODE 0x0
+#define RELEASE_MODE 0x1
+#define DEV_STATUS DEBUG_MODE // You'll have to change it later to RELEASE_MODE.
 #include "mos_graphics_vga_mode.c" 
 #endif
